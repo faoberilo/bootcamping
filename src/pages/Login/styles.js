1a192c09b0c335f0a9cb8215f29092148caf76c1
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-02-13 12:15:35 -0300

    Update styles.js

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -17,7 +17,7 @@ export const Container = styled.div`
       input{
           border-style:solid;
           border-color:black;          
-          width: 30rem;
+   
           font-size: 1.3rem;          
         }       
         div{
